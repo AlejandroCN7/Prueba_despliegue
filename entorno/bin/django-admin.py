42daba023ddc7e3ebdbad37c477e9a98d102a9f8
AlejandroCN7 <alejandroac79@gmail.com>
2018-11-07 17:16:22 +0100

    Commit inicial

---
+++ entorno/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hapneck/Escritorio/Universidad/CC/proyectos/Prueba_despliegue/entorno/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
